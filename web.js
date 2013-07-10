d4972cf57f90e8baa398396b42f4b15943122c65
hmohamm <hakeemosrc@gmail.com>
2013-07-10 23:15:49 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     contents = fs.readFileSync('./index.html');
-  response.send(contents);
+    response.send(contents.toString('utf-8');
 });
 
 var port = process.env.PORT || 5000;
